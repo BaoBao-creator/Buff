2efb2d1cd503c697d1683f66dfcefb669a761d4d
BaoBao-creator <ba0ba0113377@gmail.com>
2026-05-14 00:24:51 +0700

    Update GameRendererMixin.java

--- src/client/java/bao/buff/client/mixin/GameRendererMixin.java
+++ src/client/java/bao/buff/client/mixin/GameRendererMixin.java
@@ -1,34 +1,15 @@
 package bao.buff.client.mixin;
 
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import com.mojang.blaze3d.vertex.PoseStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
-    @Shadow @Final Minecraft minecraft;
-
     @Inject(method = "getDarkenWorldAmount", at = @At("HEAD"), cancellable = true)
     private void buff$disableWorldDarkening(float tickProgress, CallbackInfoReturnable<Float> cir) {
         cir.setReturnValue(0.0F);
-    }
-
-    @Inject(method = "bobHurt", at = @At("HEAD"), cancellable = true)
-    private void buff$disableHurtCam(PoseStack poseStack, float tickProgress, CallbackInfo ci) {
-        ci.cancel();
-    }
-
-
-    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void buff$lockFov(Camera camera, float tickProgress, boolean changingFov, CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(this.minecraft.options.fov().get().floatValue());
     }
 }
