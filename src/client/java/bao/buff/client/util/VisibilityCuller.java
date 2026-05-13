97d2e44a9f6f02409b620431a0a6b3a927f1a6fe
BaoBao-creator <ba0ba0113377@gmail.com>
2026-05-13 22:16:28 +0700

    Revert "Disable stale block-face visibility culling"

--- src/client/java/bao/buff/client/util/VisibilityCuller.java
+++ src/client/java/bao/buff/client/util/VisibilityCuller.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
@@ -16,6 +17,7 @@ import org.joml.Vector3fc;
 public final class VisibilityCuller {
     private static final double NEAR_SKIP_DISTANCE_SQ = 16.0D;
     private static final double HIT_EPSILON_SQ = 0.09D;
+    private static final double FACE_OCCLUSION_MARGIN_SQ = 0.01D;
     private static Frustum activeFrustum;
 
     private VisibilityCuller() {
@@ -39,6 +41,29 @@ public final class VisibilityCuller {
         }
 
         return isBoxVisible(entity.level(), camera.position(), camera.forwardVector(), box, null, cameraEntity);
+    }
+
+    public static boolean shouldRenderBlockFace(Direction faceDirection, BlockPos neighborPos) {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.level == null) {
+            return true;
+        }
+
+        Camera camera = minecraft.gameRenderer.getMainCamera();
+        Vec3 cameraPos = camera.position();
+        BlockPos blockPos = neighborPos.relative(faceDirection.getOpposite());
+        Vec3 faceCenter = blockFacePoint(blockPos, faceDirection, 0.5D, 0.5D);
+        if (cameraPos.distanceToSqr(faceCenter) <= NEAR_SKIP_DISTANCE_SQ) {
+            return true;
+        }
+
+        for (Vec3 sample : blockFaceSamples(blockPos, faceDirection)) {
+            if (hasBlockFaceLineOfSight(minecraft.level, cameraPos, sample, blockPos, camera.entity())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static boolean shouldRenderBlockEntity(Level level, BlockPos pos, Vec3 cameraPos) {
@@ -104,6 +129,48 @@ public final class VisibilityCuller {
 
     private static boolean hasVisibleSample(Level level, Vec3 from, double sampleX, double sampleY, double sampleZ, BlockPos targetBlock, Entity cameraEntity) {
         return hasLineOfSight(level, from, new Vec3(sampleX, sampleY, sampleZ), targetBlock, cameraEntity);
+    }
+
+    private static boolean hasBlockFaceLineOfSight(Level level, Vec3 from, Vec3 to, BlockPos targetBlock, Entity cameraEntity) {
+        BlockHitResult hit = level.clip(new ClipContext(from, to, ClipContext.Block.VISUAL, ClipContext.Fluid.NONE, cameraEntity));
+        if (hit.getType() == HitResult.Type.MISS) {
+            return true;
+        }
+
+        if (hit.getBlockPos().equals(targetBlock)) {
+            return true;
+        }
+
+        return hit.getLocation().distanceToSqr(from) + FACE_OCCLUSION_MARGIN_SQ >= to.distanceToSqr(from);
+    }
+
+    private static Vec3[] blockFaceSamples(BlockPos blockPos, Direction direction) {
+        return new Vec3[] {
+                blockFacePoint(blockPos, direction, 0.5D, 0.5D),
+                blockFacePoint(blockPos, direction, 0.2D, 0.2D),
+                blockFacePoint(blockPos, direction, 0.2D, 0.5D),
+                blockFacePoint(blockPos, direction, 0.2D, 0.8D),
+                blockFacePoint(blockPos, direction, 0.5D, 0.2D),
+                blockFacePoint(blockPos, direction, 0.5D, 0.8D),
+                blockFacePoint(blockPos, direction, 0.8D, 0.2D),
+                blockFacePoint(blockPos, direction, 0.8D, 0.5D),
+                blockFacePoint(blockPos, direction, 0.8D, 0.8D)
+        };
+    }
+
+    private static Vec3 blockFacePoint(BlockPos blockPos, Direction direction, double u, double v) {
+        double x = blockPos.getX();
+        double y = blockPos.getY();
+        double z = blockPos.getZ();
+
+        return switch (direction) {
+            case DOWN -> new Vec3(x + u, y, z + v);
+            case UP -> new Vec3(x + u, y + 1.0D, z + v);
+            case NORTH -> new Vec3(x + u, y + v, z);
+            case SOUTH -> new Vec3(x + u, y + v, z + 1.0D);
+            case WEST -> new Vec3(x, y + u, z + v);
+            case EAST -> new Vec3(x + 1.0D, y + u, z + v);
+        };
     }
 
     private static boolean hasLineOfSight(Level level, Vec3 from, Vec3 to, BlockPos targetBlock, Entity cameraEntity) {
