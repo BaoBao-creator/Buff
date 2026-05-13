171a6590316784b72c4b0c4160a7851a1396f873
BaoBao-creator <ba0ba0113377@gmail.com>
2026-05-13 19:59:00 +0700

    Fix dropped item render state age field

--- src/client/java/bao/buff/client/BuffClient.java
+++ src/client/java/bao/buff/client/BuffClient.java
@@ -6,6 +6,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class BuffClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		Buff.LOGGER.info("Buff is active: particles, sky, clouds, weather, mob animations, texture animations, and block animations disabled; fullbright forced.");
+		Buff.LOGGER.info("Buff is active: particles, sky, clouds, weather, mob animations, texture animations, block animations, and dropped-item spin disabled; dropped items use flat 2D rendering; fullbright forced.");
 	}
 }
